1ed73708e2e72967cb2092e97ec4af1a22d932c7
unknown <pradhulkariyad@gmail.com>
2024-10-25 18:35:15 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,30 @@
+// ignore_for_file: unused_import
+
+import 'package:camera/camera.dart';
 import 'package:camera_ai/views/home/home.dart';
+import 'package:camera_ai/views/mainPage.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final camera = await availableCameras();
+  runApp(MyApp(
+    cameras: camera,
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final List<CameraDescription> cameras;
+  const MyApp({super.key, required this.cameras});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Home(),
+      home: MainPage(
+        cameras: cameras,
+      ),
     );
   }
 }
---
+++ lib/views/mainPage.dart
@@ -0,0 +1,63 @@
+// ignore_for_file: file_names
+
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  final List<CameraDescription> cameras;
+  const MainPage({super.key, required this.cameras});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  late CameraController cameraController;
+  late Future<void> cameraValue;
+
+  void startCamera(int camera) {
+    cameraController = CameraController(
+        widget.cameras[camera], ResolutionPreset.high,
+        enableAudio: false);
+    cameraController.initialize();
+  }
+
+  @override
+  void initState() {
+    startCamera(0);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Stack(
+        children: [
+          FutureBuilder(
+            future: cameraValue,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                return SizedBox(
+                  width: size.width,
+                  height: size.height,
+                  child: FittedBox(
+                    fit: BoxFit.cover,
+                    child: SizedBox(
+                      width: 100,
+                      child: CameraPreview(cameraController),
+                    ),
+                  ),
+                );
+              } else {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
